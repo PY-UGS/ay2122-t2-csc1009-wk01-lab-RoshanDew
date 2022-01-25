de36d52cfdc5958719d74b37050eeac721b10fe4
RoshanDew <93643533+RoshanDew@users.noreply.github.com>
2022-01-25 16:27:49 +0800

    Add files via upload

---
+++ Wk1Q2.java
@@ -0,0 +1,16 @@
+public class Wk1Q2 
+{
+    public static void main(String arg[])
+    {
+        String module="CSC1009";
+        switch(module)
+        {
+            case"CSC1009":
+                System.out.println("Object Orientated Programming");
+                break;
+            default:
+            System.out.println("Unknown Module");;
+        }
+    
+    }
+}
---
+++ Wk1Q3.java
@@ -0,0 +1,14 @@
+public class Wk1Q3 {
+
+    public static void main(String arges[])
+    {
+        for(int x=102; x>66; x=x-1)
+        {
+            if(x%2==1){
+                System.out.println("The odd number:"+x);
+            }
+
+        }
+    }
+    
+}
